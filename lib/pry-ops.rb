fede7e7a654660fcb6c33ae113adf3fc6ff4374c
Uwe Stuehler <uwe@bsdx.de>
2013-06-19 15:28:27 +0200

    Expose some DSL elements as PryOps module methods

--- lib/pry-ops.rb
+++ lib/pry-ops.rb
@@ -2,8 +2,33 @@ require 'pry-ops/util'
 
 # +PryOps+ is a +Pry+ plugin for DevOps people.
 class PryOps
+
   extend PryOps::Util::Autoload
   autoload_all_files_relative_to __FILE__
+
+  class << self
+
+    # (see PryOps::DSL#service)
+    def service(name, *args, &block)
+      PryOps::DSL.eval do
+        service(name, *args, &block)
+      end
+    end
+
+    # (see PryOps::DSL#context)
+    def context(name, &block)
+      PryOps::DSL.eval do
+        context(name, &block)
+      end
+    end
+
+    # (see PryOps::Application#services)
+    def services
+      application.services
+    end
+
+  end # << self
+
 end
 
 # Load the PryOps DSL.
